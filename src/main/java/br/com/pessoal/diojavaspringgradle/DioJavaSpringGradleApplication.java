9c3469bc7d2f5db9bf2494636ebf703b537cacbe
VanSharine <vanessasharine@gmail.com>
2020-09-10 15:44:59 -0300

    modelo geral do uso de  gradle com spring em Java

---
+++ src/main/java/br/com/pessoal/diojavaspringgradle/DioJavaSpringGradleApplication.java
@@ -0,0 +1,13 @@
+package br.com.pessoal.diojavaspringgradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DioJavaSpringGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DioJavaSpringGradleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/pessoal/diojavaspringgradle/DioJavaSpringGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pessoal.diojavaspringgradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DioJavaSpringGradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
